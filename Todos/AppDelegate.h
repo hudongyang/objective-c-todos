93a1c0e39aef9bf27d07f82ad40509c6f3c807b6
hudongyang <hudongyang@xiaohongchun.com>
2019-04-18 10:12:27 +0800

    Initial Commit

---
+++ Todos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Todos
+//
+//  Created by hudongyang on 2019/4/18.
+//  Copyright © 2019 hudongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
